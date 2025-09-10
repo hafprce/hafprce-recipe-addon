b7681032da0e2ac79a8f55f0dcc8b7f231f9631b
hafprce <hafprce@gmail.com>
2025-09-09 17:28:34 -0700

    Initial Check In

---
+++ src/main/java/com/hafprce/recipe/addon/HafprceRecipeAddon.java
@@ -0,0 +1,16 @@
+package com.hafprce.recipe.addon;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HafprceRecipeAddon implements ModInitializer {
+	public static final String MOD_ID = "hafprce-recipe-addon";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("It's Loading UP!");
+	}
+}
---
+++ src/main/java/com/hafprce/recipe/addon/HafprceRecipeAddonClient.java
@@ -0,0 +1,10 @@
+package com.hafprce.recipe.addon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HafprceRecipeAddonClient implements ClientModInitializer {
+    @Override
+	public void onInitializeClient() {
+
+	}
+}
